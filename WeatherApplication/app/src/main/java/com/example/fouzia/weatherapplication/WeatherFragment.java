2f4836f2422ee7ef3e183f50d8bebbff0b754855
Hassan Bokhari <y2herc@gmail.com>
2017-01-16 00:44:12 -0500

    Updated Fragment Support Library

--- WeatherApplication/app/src/main/java/com/example/fouzia/weatherapplication/WeatherFragment.java
+++ WeatherApplication/app/src/main/java/com/example/fouzia/weatherapplication/WeatherFragment.java
@@ -1,7 +1,8 @@
 package com.example.fouzia.weatherapplication;
 
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
